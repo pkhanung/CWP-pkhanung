daeda718a8a28fdb700396bf4cb3cd30d4c0f72c
root <root@Ms-202509071400.localdomain>
2026-09-23 01:23:28 +0700

    Add advanced_mult

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+else:
+    i = 0
+    while i <= 10:
+        print("Table de", i, ":", end="")
+        j = 0
+        while j <= 10:
+            print(" ", i * j, end="")
+            j += 1
+        print()
+        i += 1

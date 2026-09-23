14ffd8069149e8ed2a3b0db4cdcc13aef4ffaf9b
root <root@Ms-202509071400.localdomain>
2026-09-23 18:23:00 +0700

    Add family_affairs exercise

---
+++ cell07/ex01/family_affairs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def find_the_redheads(family):
+    return list(filter(lambda name: family[name] == "red", family.keys()))
+
+
+dupont_family = {
+    "florian": "red",
+    "marie": "blond",
+    "virginie": "brunette",
+    "david": "red",
+    "franck": "red"
+}
+
+print(find_the_redheads(dupont_family))

43fe22927a1a939f7986dfa77edb99888be446ba
root <root@Ms-202509071400.localdomain>
2026-09-23 13:06:25 +0700

    add cell05 ex03

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+new_array = []
+
+for number in original_array:
+    if number > 5:
+        new_array.append(number + 2)
+
+new_array = set(new_array)
+
+print(original_array)
+print(new_array)

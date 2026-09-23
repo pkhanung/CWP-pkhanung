4e7b2c4a298015c022d40284f0ae665cfb0c4e23
root <root@Ms-202509071400.localdomain>
2026-09-23 15:11:56 +0700

    Add ex14 free_range

---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) != 3:
+    print("none")
+else:
+    start = int(sys.argv[1])
+    end = int(sys.argv[2])
+    arr = list(range(start, end + 1))
+    print(arr)
